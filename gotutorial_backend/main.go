a108e94b78ac5d8748560bdc3852650ed663e7e6
Beata <beata@jasinski.com>
2024-07-02 20:36:34 -0700

    using packages

---
+++ gotutorial_backend/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "example.com/gotutorial/test"
+
+func main() {
+	test.MyFunction()
+}
---
+++ gotutorial_backend/test/test.go
@@ -0,0 +1,10 @@
+package test
+
+import "fmt"
+
+func MyFunction() {
+	fmt.Println("step1")
+	fmt.Println("step2")
+	fmt.Println("step3")
+	fmt.Println("step4")
+}
